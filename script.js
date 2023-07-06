594c441cfeea55a6845ced0ea1d052a4307dcd16
trippytresor <104313854+trippytresor@users.noreply.github.com>
2023-07-06 21:17:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
+function changeSection(sectionId) {
+  var sections = document.querySelectorAll('.section');
+  var video = document.getElementById('player');
 
-  function changeSection(sectionId) {
-    var sections = document.querySelectorAll('.section');
-    for (var i = 0; i < sections.length; i++) {
-      if (sections[i].id === sectionId) {
-        sections[i].style.display = 'block';
-      } else {
-        sections[i].style.display = 'none';
+  for (var i = 0; i < sections.length; i++) {
+    if (sections[i].id === sectionId) {
+      sections[i].style.display = 'block';
+      if (sections[i].contains(video)) {
+        video.play(); // Resume video playback
+      }
+    } else {
+      sections[i].style.display = 'none';
+      if (sections[i].contains(video)) {
+        video.pause(); // Pause video playback
       }
     }
   }
-  
-
-  
-  // get the video element
-const myVideo = document.getElementById("player");
-
-// stop video playback when switching to a different section
-function switchSection(newSection) {
-  player.pause();
-  document.getElementById("section0").style.display = "none";
-  document.getElementById("section1").style.display = "none";
-  document.getElementById("section2").style.display = "none";
-  document.getElementById("section3").style.display = "none";
-  document.getElementById("section4").style.display = "none";
-  document.getElementById("section5").style.display = "none";
-  document.getElementById("section6").style.display = "none";
-  document.getElementById("section7").style.display = "none";
-  document.getElementById("section8").style.display = "none";
-  document.getElementById("section9").style.display = "none";
-  document.getElementById("section" + newSection).style.display = "block";
 }
 
+// Attach event listener to detect section changes
+document.addEventListener('DOMContentLoaded', function() {
+  var sectionLinks = document.querySelectorAll('.section-link');
+  for (var i = 0; i < sectionLinks.length; i++) {
+    sectionLinks[i].addEventListener('click', function(event) {
+      var sectionId = event.target.getAttribute('data-section');
+      changeSection(sectionId);
+    });
+  }
+});
 
-function scrollToSection(containerId) {
-  const contrainer = document.getElementById(containerId);
-  container.scrollIntoView({behavior: 'smooth'});
+//Scroll to section
+function scrollToSection(sectionId) {
+  const targetSection = document.getElementById(sectionId);
+
+  if (targetSection) {
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
 }
-  
-  
-  
-  
-  
-  
-  
-
-
